16d46f839691d7e06bd90c52333d5f2050e76e58
Ansyariadimaputra <ansyariputra01@gmail.com>
2023-06-17 14:40:13 +0700

    Penambahan template

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <title>Detail Data Post - Belajar Laravel 9</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body style="background: lightgray">
 
     <div class="container mt-5 mb-5">
@@ -14,7 +16,7 @@
             <div class="col-md-8">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
-                        <img src="{{ asset('storage/posts/'.$post->image) }}" class="w-100 rounded">
+                        <img src="{{ asset('storage/posts/' . $post->image) }}" class="w-100 rounded">
                         <hr>
                         <h4>{{ $post->title }}</h4>
                         <p class="tmt-3">
@@ -29,4 +31,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
